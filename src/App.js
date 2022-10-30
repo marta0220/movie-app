16aa7c283bb39213e085a34eba9e0af06edd7d3d
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-30 14:56:09 +0200

    Divide App.js into parts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Movie from "./Movie";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header></header>
+      <Movie />
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
